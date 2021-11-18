31df9cfb1f0a45a2daf95301a940ff9e831c7985
kamilb22 <222574@edu.p.lodz.pl>
2021-11-18 15:10:33 +0100

    implemented Api3 methods

--- src/main/java/com/example/api4/Controller.java
+++ src/main/java/com/example/api4/Controller.java
@@ -3,10 +3,7 @@ package com.example.api4;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -20,10 +17,30 @@ public class Controller {
         final String uri = "http://localhost:8081" + string;
         RestTemplate restTemplate = new RestTemplate();
         UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(uri)
-                .queryParam("mode",mode);
+                .queryParam("mode", mode);
 
-        return restTemplate.exchange(builder.buildAndExpand().toUri() , HttpMethod.GET,
+        return restTemplate.exchange(builder.buildAndExpand().toUri(), HttpMethod.GET,
                 new HttpEntity(new HttpHeaders()), String.class).getBody();
+    }
+
+    @RequestMapping("/lowerCase/{string}")
+    public String lowerCase(@PathVariable String string, @RequestParam(required = false) String mode) {
+        return getResponseFromAnotherApi("/lowerCase/" + string, mode);
+    }
+
+    @RequestMapping("/upperCase/{string}")
+    public String upperCase(@PathVariable String string, @RequestParam(required = false) String mode) {
+        return getResponseFromAnotherApi("/upperCase/" + string, mode);
+    }
+
+    @RequestMapping("/numbers/{string}")
+    public String numbers(@PathVariable String string, @RequestParam(required = false) String mode) {
+        return getResponseFromAnotherApi("/numbers/" + string, mode);
+    }
+
+    @RequestMapping("/special/{string}")
+    public String special(@PathVariable String string, @RequestParam(required = false) String mode) {
+        return getResponseFromAnotherApi("/special/" + string, mode);
     }
 
 
