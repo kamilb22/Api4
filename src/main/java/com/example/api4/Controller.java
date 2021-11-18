0c3166a6e2096c139282fe95ec134aafc8e36c37
kamilb22 <222574@edu.p.lodz.pl>
2021-11-18 15:38:42 +0100

    implemented convert method

--- src/main/java/com/example/api4/Controller.java
+++ src/main/java/com/example/api4/Controller.java
@@ -1,5 +1,7 @@
 package com.example.api4;
 
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -43,5 +45,42 @@ public class Controller {
         return getResponseFromAnotherApi("/special/" + string, mode);
     }
 
+    @RequestMapping("/convert")
+    public String convert(@RequestBody String string, @RequestParam(required = false) String inputMode, @RequestParam(required = false) String outputMode) {
+        int result = 0;
+        if (inputMode.equals("json")) {
+            JSONObject js = new JSONObject(string);
+            result = js.getInt("result");
+        }
+        if (inputMode.equals("xml")) {
+            JSONObject js = XML.toJSONObject(string);
+            result = js.getInt("result");
+        }
+        if (inputMode.equals("csv")){
+            String num = string.substring(8);
+            result = Integer.parseInt(num);
+        }
+        if (inputMode.equals("txt")) {
+            String[] temp = string.split(" ");
+            result = Integer.parseInt(temp[1]);
+        }
+
+
+        if (outputMode.equals("json")) {
+            return new JSONObject("{ \"result\" : " + result + " }").toString();
+        }
+        if (outputMode.equals("xml")) {
+            return "<result>" + result + "<result>";
+        }
+        if (outputMode.equals("csv")){
+            return "\"result\"\n" + result;
+        }
+        if (outputMode.equals("txt")) {
+            return "result: " + result;
+        }
+
+
+        return Integer.toString(result);
+    }
 
 }
